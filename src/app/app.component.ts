7833fd65da6f849fd9569df054a0147e8e017d41
Латышева Александра <alatysheva@diasoft.ru>
2024-03-27 15:49:46 +0300

    module 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,8 +8,7 @@ import { HeaderComponent } from './components/header/header.component';
   standalone: true,
   imports: [RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'courses-page';
 }
